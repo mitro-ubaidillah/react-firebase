b8ca943da9a8b54f5ee1c593c8609a958773297b
mitro-ubaidillah <ubaidillahmitro@gmail.com>
2024-03-31 11:32:42 +0700

    initial setup

---
+++ src/components/example/button/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ButtonExample: React.FC = () => {
+  return <button>Example Button</button>;
+};
+
+export default ButtonExample;
---
+++ src/components/example/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ExampleImportAlias: React.FC = () => {
+  return (
+    <div>
+      <h1>Example Component</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, ipsa
+        error dolore molestias fugiat atque doloribus sunt deserunt
+        exercitationem ab vel modi officia officiis molestiae. Excepturi
+        perferendis quidem doloribus sapiente!
+      </p>
+    </div>
+  );
+};
+
+export default ExampleImportAlias;
